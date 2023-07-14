245dd05ee2b89a3dabce282d9994612e41b5bf7a
kenken524 <ken5240719@gmail.com>
2023-07-14 09:43:02 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,11 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
+  end
 
   with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角文字(カタカナ)を使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
+  end
 
 end
