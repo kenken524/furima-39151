be894912fcfe4008ccbbc278fd17828b6c10d3d3
kenken524 <ken5240719@gmail.com>
2023-07-14 09:41:58 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,:birthday, presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角文字(カタカナ)を使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
 
 end
