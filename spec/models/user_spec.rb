9ba8e1d813032f5acd13a5d2fb148b44b5ecd249
kenken524 <ken5240719@gmail.com>
2023-07-16 14:47:56 +0900

    コード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,16 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'testtest'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'テストテスト'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '1234567'
