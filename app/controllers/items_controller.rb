21d07d45c11129e84e625594807be5791de1a0ce
kenken524 <ken5240719@gmail.com>
2023-07-17 00:40:34 +0900

    コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,19 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+
+
   private
 
-  def message_params
+  def item_params
     params.require(:item).permit(:image, :name, :price, :category_id, :status_id, :delivery_cost_id, :prefecture_id, :day_to_ship_id).merge(user_id: current_user.id)
   end
 end
