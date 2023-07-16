5610d424efa977eafde278444480d477d2a255a0
kenken524 <ken5240719@gmail.com>
2023-07-16 19:24:31 +0900

    activehashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
   belongs_to :user
+
+
+
+  validates :category_id, :status_id, :delivery_cost_id, :prefecture_id, :day_to_ship_id, numericality: { other_than: 1 } 
 end
