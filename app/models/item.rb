1d8c796dae3fff139f60f108282692a90d0a745d
kenken524 <ken5240719@gmail.com>
2023-07-16 19:36:26 +0900

    activestorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :category_id, :status_id, :delivery_cost_id,:prefecture_id, :day_to_ship_id
   belongs_to :user
+  has_one_attached :image
 
 
 
-  validates :category_id, :status_id, :delivery_cost_id, :prefecture_id, :day_to_ship_id, numericality: { other_than: 1 } 
+
+  validates :category_id, :status_id, :delivery_cost_id, :prefecture_id, :day_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
