7975a84ae5e3e5c9607638f62e1dac9a58fe64da
kenken524 <ken5240719@gmail.com>
2023-07-17 21:34:36 +0900

    出品リンクとカテゴリの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id, :status_id, :delivery_cost_id,:prefecture_id, :day_to_ship_id
+  belongs_to :category_id
+  belongs_to :status_id
+  belongs_to :delivery_cost_id
+  belongs_to :prefecture_id
+  belongs_to :day_to_ship_id
   belongs_to :user
   has_one_attached :image
 
-  validates :image, :name, :price, :category_id, :status_id, :delivery_cost_id, :prefecture_id, :day_to_ship_id, presence: true
+  validates :image, :name, :category_id, :status_id, :delivery_cost_id, :prefecture_id, :day_to_ship_id, presence: true
   validates :category_id, :status_id, :delivery_cost_id, :prefecture_id, :day_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: " : Please input 300~9999999" }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: " : Please input 300~9999999" }
   validates :price, format: { with: /\A\d+\z/, message: "は半角数値のみ入力してください" }
 end
