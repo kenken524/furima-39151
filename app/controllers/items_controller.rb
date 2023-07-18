3bc7a13ad3bb80f7f70a0631e27c3e6a5181a8d6
kenken524 <ken5240719@gmail.com>
2023-07-18 22:09:35 +0900

    rubocopp

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,11 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :info, :price, :category_id, :status_id, :delivery_cost_id, :prefecture_id, :day_to_ship_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :info, :price, :category_id, :status_id, :delivery_cost_id, :prefecture_id,
+                                 :day_to_ship_id).merge(user_id: current_user.id)
   end
 end
