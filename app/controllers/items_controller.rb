33b2b151a0934994a24faec478a0c8e68f5a04e5
kenken524 <ken5240719@gmail.com>
2023-07-26 09:26:05 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,9 +38,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    return unless item.user == current_user
+    item.destroy if item.user == current_user
 
-    item.destroy
     redirect_to root_path
   end
 
